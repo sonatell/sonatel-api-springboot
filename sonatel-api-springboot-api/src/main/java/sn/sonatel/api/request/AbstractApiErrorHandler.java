5946993ee7d8d6b7e329d371b55c496dcd459f5c
Komi Serge Innocent [GEBEYA DMGP/DD/DPD] <sergekomi.innocent@orange-sonatel.com>
2022-11-02 15:37:01 +0000

    Provide abstractions to handle http requests and exceptions in general
    Added an ApiException class that maps exception to a more readable one for consumers with messages and codes from api
    
    Added an interface to handle all sorts of exceptions that may occur while making an HTTP call and converting them into some instance of ApiException

---
+++ sonatel-api-springboot-api/src/main/java/sn/sonatel/api/request/AbstractApiErrorHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sn.sonatel.api.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.reactive.function.client.WebClientException;
+import sn.sonatel.api.exceptions.ApiError;
+import sn.sonatel.api.exceptions.ApiException;
+
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * @author Komi Serge Innocent
+ */
+public abstract class AbstractApiErrorHandler<T extends ApiException> extends
+		AbstractRequestErrorHandler<T> {
+
+	private final ObjectMapper objectMapper;
+
+	protected AbstractApiErrorHandler(Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
+		this.objectMapper = jackson2ObjectMapperBuilder.build();
+	}
+
+	protected final ApiException httpResponseErrorToBaseException(
+			WebClientException e) {
+		ApiException base = super.httpResponseErrorToBaseException(e);
+		ApiError parsedError = this.parseErrorResponse(base.getMessage()).orElse(null);
+		return  ApiException.createException(base.getErrorCode(), base.getMessage(), e.getCause(), parsedError);
+	}
+
+	private Optional<ApiError> parseErrorResponse(String content) {
+		ApiError response = null;
+
+		if (content != null && !content.isEmpty()) {
+			try {
+				response = objectMapper.readValue(content.substring(content.indexOf("{")), ApiError.class);
+			}
+			catch (IOException | IndexOutOfBoundsException e) {
+				// Ignore any error that may occur while parsing the error response. The server
+				// may have responded with a non-json payload. Return an empty return value, and
+				// let the base class logic come into play.
+			}
+		}
+
+		return Optional.ofNullable(response);
+	}
+}
