9d46f85255113d0a7a6dfb50656857b29f23a544
Mandiaye DIOP <mandiaye.diop@orange-sonatel.com>
2022-11-07 16:57:01 +0000

    Add licences

--- sonatel-api-springboot-api/src/main/java/sn/sonatel/api/service/TransactionService.java
+++ sonatel-api-springboot-api/src/main/java/sn/sonatel/api/service/TransactionService.java
@@ -3,9 +3,10 @@ package sn.sonatel.api.service;
 import sn.sonatel.api.model.PublicKey;
 import sn.sonatel.api.model.TransactionRequest;
 import sn.sonatel.api.model.TransactionResponse;
+import sn.sonatel.api.model.exception.ClientResponseException;
 
 public interface TransactionService {
-        TransactionResponse cashIn(TransactionRequest request);
+        TransactionResponse cashIn(TransactionRequest request) throws ClientResponseException;
         PublicKey getPublicKey();
 
 }
